aa24a4e14c87a2df7f8950fbca4dd724038b6996
spin6lock <john.37@gmail.com>
2015-08-22 17:55:50 +0800

    use ttl in dns response now, default is 60 seconds

--- cache.py
+++ cache.py
@@ -34,6 +34,7 @@ def unpack_name(rawstring, offset):
         if label_length >= 192: #this is pointer
             label_length = struct.unpack(">H", rawstring[i:i+2])[0]
             pointer_offset = label_length & 0x3fff
+            logger.debug("pointer offset:%d", pointer_offset)
             labels, _ = unpack_name(rawstring, pointer_offset)
             return labels, i + 2
         else:
@@ -56,26 +57,45 @@ class memorized_domain(memorized):
             value, last_update = cache[url]
             value = raw_data[0:2] + value[2:]
             logger.debug("hit cache")
-            if now - last_update > self.ttl:
+            if now > last_update:
                 raise AttributeError
         except (KeyError, AttributeError):
             logger.debug("miss cache")
             value = self.func(obj, raw_data)
-            cache[url] = (value, now)
+            ttl = self.extract_ttl(value)
+            cache[url] = (value, now + ttl)
             self.cache[self.cache_name] = cache
         return value
 
     def extract_url(self, data):
         logger.debug("raw dns request:%s", data.encode('hex'))
         #pass 12 bytes
-        data = data[12:]
+        start = 12
         #extract the url
         url = []
-        start = 0
         len_of_data = ord(data[start])
         logger.debug("len of data:%s", len_of_data)
-        labels, offset = unpack_name(data, 0)
+        labels, offset = unpack_name(data, start)
         url = ".".join(labels)
         logger.debug("requesting url:%s", url)
         return url
+
+    def extract_ttl(self, data):
+        logger.debug("raw dns response:%s", data.encode('hex'))
+        start = 12
+        _, offset = unpack_name(data, start)
+        query_type_len = 2
+        query_class_len = 2
+        start = offset + query_type_len + query_class_len
+        logger.debug("start:%d, %s", start, data[start:].encode("hex"))
+        _, offset = unpack_name(data, start)
+        logger.debug("offset:%d", offset)
+        logger.debug("type class ttl:%s", data[offset:offset+8].encode("hex"))
+        ret = struct.unpack("!HHI", data[offset:offset+8])
+        dns_type, dns_class, ttl = ret
+        logger.debug("ttl:%s", ttl)
+        if ttl == 0:
+            ttl = 60 #in secs
+        return ttl
+
 
