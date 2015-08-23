3832186ffdd163441ae39563feb59c9ef8f65097
spin6lock <john.37@gmail.com>
2015-08-23 20:40:50 +0800

    fix code format

--- http_dns.py
+++ http_dns.py
@@ -12,7 +12,9 @@ from common import AUTHORIZED_DNS_SERVER, TIMEOUT, REMOTE_TCP_DNS_PORT
 
 logger = logging.getLogger('http_dns')
 
-def pack_dns_header(_id, QR, Opcode, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT):
+
+def pack_dns_header(_id, QR, Opcode, AA, TC, RD, RA, Z, RCODE, QDCOUNT,
+                    ANCOUNT, NSCOUNT, ARCOUNT):
     QR = QR << 15
     Opcode = Opcode << 11
     AA = AA << 10
@@ -29,27 +31,29 @@ def pack_dns_header(_id, QR, Opcode, AA, TC, RD, RA, Z, RCODE, QDCOUNT, ANCOUNT,
     ARCOUNT = struct.pack(">h", ARCOUNT)
     return header_id + header_operator + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT
 
+
 def unpack_dns_header(rawstr):
     ID_SIZE = 2
     HEADER_SIZE = 2
     COUNT_FIELD_SIZE = 2 * 4
     start = 0
-    _id = struct.unpack(">h", rawstr[start:start+ID_SIZE])[0]
+    _id = struct.unpack(">h", rawstr[start:start + ID_SIZE])[0]
     start += ID_SIZE
-    header_operator = struct.unpack(">h", rawstr[start:start+HEADER_SIZE])[0]
+    header_operator = struct.unpack(">h", rawstr[start:start + HEADER_SIZE])[0]
     start += HEADER_SIZE
-    QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = struct.unpack(">hhhh", rawstr[start:start+COUNT_FIELD_SIZE])
+    QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT = struct.unpack(
+        ">hhhh", rawstr[start:start + COUNT_FIELD_SIZE])
     start += COUNT_FIELD_SIZE
-    QR = (header_operator & 1<<15) >> 15
+    QR = (header_operator & 1 << 15) >> 15
     Opcode = (header_operator & 0b1111 << 11) >> 11
     AA = (header_operator & 1 << 10) >> 10
-    TC = (header_operator & 1<< 9) >> 9
-    RD = (header_operator & 1<< 8) >> 8
-    RA = (header_operator & 1<< 7) >> 7
-    Z = (header_operator & 0b111<< 4) >> 4
+    TC = (header_operator & 1 << 9) >> 9
+    RD = (header_operator & 1 << 8) >> 8
+    RA = (header_operator & 1 << 7) >> 7
+    Z = (header_operator & 0b111 << 4) >> 4
     RCODE = header_operator & 0b1111
     return {
-            "id" : _id, 
+            "id" : _id,
             "QDCOUNT" : QDCOUNT,
             "ANCOUNT" : ANCOUNT,
             "NSCOUNT" : NSCOUNT,
@@ -62,59 +66,64 @@ def httpdns_request(domain_name):
     resp = resp.split(";")
     return resp
 
+
 def construct_resp_header(dns_header, resp_len):
     kwparas = dict(
-        _id = dns_header["id"],
-        QR = 1, #this is response
-        Opcode = 0, #standard query
-        AA = 0, # not an authority for domain
-        TC = 0, # no truncated
-        RD = dns_header["header"]["RD"], # recursion desire
-        RA = 1, # server can do recursive look up
-        Z = 0, # reserved
-        RCODE = 0, #no error 
-        QDCOUNT = dns_header["QDCOUNT"],
-        ANCOUNT = resp_len,
-        ARCOUNT = 0,
-        NSCOUNT = 0,
-    )
+        _id=dns_header["id"],
+        QR=1,  #this is response
+        Opcode=0,  #standard query
+        AA=0,  # not an authority for domain
+        TC=0,  # no truncated
+        RD=dns_header["header"]["RD"],  # recursion desire
+        RA=1,  # server can do recursive look up
+        Z=0,  # reserved
+        RCODE=0,  #no error 
+        QDCOUNT=dns_header["QDCOUNT"],
+        ANCOUNT=resp_len,
+        ARCOUNT=0,
+        NSCOUNT=0, )
     return pack_dns_header(**kwparas)
+
 
 def pack_a_record(name_offset, ip):
     pointer_mark = 0b11 << (2 + 4 * 3)
-    pointer = struct.pack("!H", pointer_mark | name_offset) 
-    record_type = struct.pack("!H", 1) #type A, host address
-    record_class = struct.pack("!H", 1) #class IN, internet
+    pointer = struct.pack("!H", pointer_mark | name_offset)
+    record_type = struct.pack("!H", 1)  #type A, host address
+    record_class = struct.pack("!H", 1)  #class IN, internet
     ttl = struct.pack("!I", 60)
     data_length = struct.pack("!H", 4)
     packed_ip = socket.inet_aton(ip)
     return pointer + record_type + record_class + ttl + data_length + packed_ip
-    
+
+
 class HttpDnsHandle(object):
     def http_response(self, dns_req):
         logger.debug("req :%s", dns_req.encode("hex"))
-        dns_header,dlen = unpack_dns_header(dns_req)
+        dns_header, dlen = unpack_dns_header(dns_req)
         name_start = dlen
-        labels, name_end = unpack_name(dns_req, name_start) 
+        labels, name_end = unpack_name(dns_req, name_start)
         domain_name = '.'.join(labels)
         resp = httpdns_request(domain_name)
         logger.debug("ips:%s", str(resp))
         resp_header = construct_resp_header(dns_header, len(resp))
         QTYPE_SIZE = 2
         QCLASS_SIZE = 2
-        queries = dns_req[name_start:name_end+QTYPE_SIZE+QCLASS_SIZE]
+        queries = dns_req[name_start:name_end + QTYPE_SIZE + QCLASS_SIZE]
         records = ''.join([pack_a_record(name_start, r) for r in resp])
         dns_resp = resp_header + queries + records
         logger.debug("resp:%s", dns_resp.encode("hex"))
         return dns_resp
 
+
 def test_http_dns():
-    result = "038b818000010001000000000377777706616d617a6f6e03636f6d00c00c000100010000003c0004b02067cd".decode("hex")
+    result = "038b818000010001000000000377777706616d617a6f6e03636f6d00c00c000100010000003c0004b02067cd".decode(
+        "hex")
     with open("dns_req", "r") as fin:
         dns_req = fin.read()
         h = HttpDnsHandle()
         dns_resp = h.http_response(dns_req)
-    assert(dns_resp == result)
+    assert (dns_resp == result)
+
 
 if __name__ == "__main__":
     test_http_dns()
