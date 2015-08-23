3eb229ebd74f83816f1e0443ff7546ce183a617e
spin6lock <john.37@gmail.com>
2015-08-23 11:25:16 +0800

    fix tcp connection pool problem
    it doesn't cache conn before :(

--- tcp_dns.py
+++ tcp_dns.py
@@ -5,15 +5,16 @@ import logging
 import Queue
 
 import cache
-from common import AUTHORIZED_DNS_SERVER, TIMEOUT, REMOTE_TCP_DNS_PORT
+from common import AUTHORIZED_DNS_SERVER, TIMEOUT, REMOTE_TCP_DNS_PORT, TCP_QUEUE_SIZE
 
 logger = logging.getLogger('tcp_dns')
 
-tcp_pool = Queue.Queue()
 class TCP_Handle(object):
     @cache.memorized_domain
     def tcp_response(self, data):
         """ tcp request dns data """        
+        if not hasattr(self, "tcp_pool"):
+            self.tcp_pool = Queue.Queue(maxsize=TCP_QUEUE_SIZE)
         resp = None
         while not resp:
           sock = self.get_tcp_sock()
@@ -43,7 +44,8 @@ class TCP_Handle(object):
 
     def get_tcp_sock(self):
         try:
-          sock = tcp_pool.get(block=True, timeout=TIMEOUT)
+          logger.debug("tcp pool size:%d", self.tcp_pool.qsize())
+          sock = self.tcp_pool.get(block=False)
         except Queue.Empty:
           logger.debug("tcp pool is empty, now create a new socket")
           sock = self.create_tcp_sock()
@@ -51,13 +53,13 @@ class TCP_Handle(object):
     
     def release_tcp_sock(self, sock):
         try:
-          tcp_pool.put(sock, block=False)
+          self.tcp_pool.put(sock, block=False)
         except Queue.Full:
           logger.debug("tcp pool is full, now throw away the oldest socket")
-          old_sock = tcp_pool.get(block=False)
+          old_sock = self.tcp_pool.get(block=False)
           old_sock.close()
           logger.debug("close sock")
-          tcp_pool.put(sock, block=False)
+          self.tcp_pool.put(sock, block=False)
 
     def create_tcp_sock(self):
         logger.debug("create sock")
