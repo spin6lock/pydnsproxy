e636f6235d87fb2c32c734c09bda9f7ceef9b036
spin6lock <spin6lock@users.noreply.github.com>
2017-03-01 16:01:02 +0800

    only one tcp socket to server at the same time

--- tcp_dns.py
+++ tcp_dns.py
@@ -1,44 +1,54 @@
 # -*- encoding: utf-8 -*-
 import struct
 import socket
+from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM, timeout
 import logging
 import Queue
 import errno
+from gevent._semaphore import BoundedSemaphore
 
 import cache
 from common import AUTHORIZED_DNS_SERVER, TIMEOUT, REMOTE_TCP_DNS_PORT, TCP_QUEUE_SIZE
 
 logger = logging.getLogger('tcp_dns')
 
+sem = BoundedSemaphore(TCP_QUEUE_SIZE)
 
 class TCP_Handle(object):
     @cache.memorized_domain
     def tcp_response(self, data):
         """ tcp request dns data """
-        if not hasattr(self, "tcp_pool"):
-            self.tcp_pool = Queue.Queue(maxsize=TCP_QUEUE_SIZE)
         resp = None
-        sock = self.get_tcp_sock()
+        if not hasattr(self, "sock"):
+            self.sock = self.create_tcp_sock()
         size_data = self.tcp_packet_head(data)
         packet = size_data + data
+        new_sock = socket.socket(AF_INET, SOCK_DGRAM)  # socket for the test DNS server
+        new_sock.connect((AUTHORIZED_DNS_SERVER, 53))
+        new_sock.sendall(data)
+        sem.acquire()
         while not resp:
             try:
-                sock.send(packet)
-                resp = sock.recv(1024)
+                self.sock.send(packet)
+                resp = self.sock.recv(1024)
                 logger.debug("receive data:[%d]%s",
                              len(resp), resp.encode('hex'))
             except socket.timeout:
                 logger.error("tcp socket timeout, throw away")
-                self.drop_tcp_sock(sock)
-                sock = self.get_tcp_sock()
+                self.drop_tcp_sock(self.sock)
+                self.sock = self.get_tcp_sock()
             except IOError, e:
                 if e.errno == errno.EPIPE:
                     logger.error("tcp socket broken pipe, throw away")
-                    self.drop_tcp_sock(sock)
-                    sock = self.get_tcp_sock()
+                    self.drop_tcp_sock(self.sock)
+                    self.sock = self.get_tcp_sock()
+                elif e.errno == errno.EBADF:
+                    logger.error("remote server close socket")
+                    self.drop_tcp_sock(self.sock)
+                    self.sock = self.get_tcp_sock()
                 else:
                     raise e
-        self.release_tcp_sock(sock)
+        sem.release()
         return self.packet_body(resp)
 
     def tcp_packet_head(self, data):
@@ -55,6 +65,7 @@ class TCP_Handle(object):
         return data[2:2 + size]
 
     def get_tcp_sock(self):
+        """
         try:
             logger.debug("tcp pool size:%d", self.tcp_pool.qsize())
             sock = self.tcp_pool.get(block=False)
@@ -62,6 +73,8 @@ class TCP_Handle(object):
             logger.debug("tcp pool is empty, now create a new socket")
             sock = self.create_tcp_sock()
         return sock
+        """
+        return self.create_tcp_sock()
 
     def drop_tcp_sock(self, sock):
         logger.debug("close timeout sock")
@@ -79,7 +92,7 @@ class TCP_Handle(object):
 
     def create_tcp_sock(self):
         logger.debug("create sock")
-        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock = socket.socket(AF_INET, SOCK_STREAM)
         tcp_sock.connect((AUTHORIZED_DNS_SERVER, REMOTE_TCP_DNS_PORT))
         tcp_sock.settimeout(TIMEOUT)
         return tcp_sock
